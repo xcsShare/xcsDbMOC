3ab8da70c9cebbbfac71b37d95aaa1a02c134433
Cypress <cypressmobileapp@gmail.com>
2018-10-21 23:54:10 +0800

    Build v0.1.1.2

--- xcsDbMOC.podspec
+++ xcsDbMOC.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'xcsDbMOC'
-  s.version          = '0.1.1'
+  s.version          = '0.1.1.2'
   s.summary          = 'A short description of xcsDbMOC.'
 
 # This description is used to generate tags and improve search results.
---
+++ xcsDbMOC/Classes/DbMOC.framework/Headers/DbMOC.h
@@ -0,0 +1,21 @@
+//
+//  DbMOC.h
+//  DbMOC
+//
+//  Created by Cypress on 2018/10/19.
+//  Copyright © 2018 xcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "agTagObject.h"
+#import "agCoreDataMOC.h"
+
+//! Project version number for DbMOC.
+FOUNDATION_EXPORT double DbMOCVersionNumber;
+
+//! Project version string for DbMOC.
+FOUNDATION_EXPORT const unsigned char DbMOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DbMOC/PublicHeader.h>
+
+
